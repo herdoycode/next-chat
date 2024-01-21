6536797a88ef3f1cf1fc279b1425e309f11d61e3
Herdoy Almamun <herdoy1@gmail.com>
2024-01-21 11:23:57 +0600

    Create responsive layout

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import { Box, Grid } from "@radix-ui/themes";
+
+const Dashboard = () => {
+  return (
+    <Grid
+      columns={{ md: "70px 300px 1fr", initial: "1fr" }}
+      className="h-screen"
+    >
+      <Box
+        position={{ initial: "fixed", md: "static" }}
+        bottom={{ initial: "0", md: "auto" }}
+        width={{ initial: "100%", md: "auto" }}
+        height={{ initial: "9", md: "auto" }}
+        className="bg-gray-200"
+      >
+        menu
+      </Box>
+      <Box className="bg-gray-100">aside</Box>
+      <Box
+        position={{ initial: "fixed", md: "static" }}
+        right={{ initial: "100%", md: "auto" }}
+        width="100%"
+      >
+        messages
+      </Box>
+    </Grid>
+  );
+};
+
+export default Dashboard;
